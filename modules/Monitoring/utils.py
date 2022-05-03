63e944aeee07848aaddaffd205c119d9dae88731
Yoann-Renard <yoann.rnrd@gmail.com>
2022-05-03 19:04:57 +0200

    monitoring and settings

--- modules/Monitoring/utils.py
+++ modules/Monitoring/utils.py
@@ -1,14 +1,34 @@
 from pynput import keyboard
 from modules.Timer import *
+from modules.Logging import *
+from modules.Utils import *
+
+chain_buffer = []
+intervals_buffer = []
 
 
-def on_press(key):
-    try:
-        print(key.char, end='')
-        print(TIMER.step())
-    except AttributeError:
-        print(key, end='')
-        print(TIMER.step())
+def writeln() -> None:
+    global chain_buffer
+    global intervals_buffer
+    if chain_buffer and intervals_buffer:
+        writer.info(msg=f'"{str(chain_buffer)}","{str(intervals_buffer)}"')
+        chain_buffer = []
+        intervals_buffer = []
+
+
+def on_press(key) -> None:
+    step = TIMER.step()
+    if str(type(key)) != "<enum 'Key'>":
+        if key.char == "'":
+            return None
+    if step == 0.0:
+        writeln()
+    else:
+        intervals_buffer.append(step)
+    if str(type(key)) == "<enum 'Key'>":
+        chain_buffer.append(str(key))
+    else:
+        chain_buffer.append(key)
 
 
 def start_input_monitoring() -> None:
--- modules/Settings/writing.py
+++ modules/Settings/writing.py
@@ -1,3 +1,4 @@
 WRITER_FILE = "data.csv"
 WRITER_FORMAT = "%(message)s"
 WRITER_FILEMODE = 'a'
+WRITER_LEVEL = 20  # Info
