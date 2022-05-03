b9627afb98bc43e781e74a3b1e340be41a139a8c
Yoann-Renard <yoann.rnrd@gmail.com>
2022-05-03 19:04:43 +0200

    logging update

--- modules/Logging/__init__.py
+++ modules/Logging/__init__.py
@@ -1,2 +1,11 @@
+from os import stat
+
 from .logger import *
 from .writer import *
+
+from modules.Settings import WRITER_FILE
+
+if stat(WRITER_FILE).st_size == 0:
+    with open(WRITER_FILE, 'a') as f:
+        f.write("character_chain, intervals\n")
+        f.close()
--- modules/Logging/writer.py
+++ modules/Logging/writer.py
@@ -1,9 +1,10 @@
 import logging
 
-from modules.Settings import WRITER_FILEMODE, WRITER_FORMAT, WRITER_FILE
+from modules.Settings import WRITER_FILEMODE, WRITER_FORMAT, WRITER_FILE, WRITER_LEVEL
 
 writer = logging.getLogger("writer")
 
 writer_handler = logging.FileHandler(filename=WRITER_FILE, mode=WRITER_FILEMODE)
 writer_handler.setFormatter(logging.Formatter(WRITER_FORMAT))
+writer.setLevel(level=WRITER_LEVEL)
 writer.addHandler(writer_handler)
